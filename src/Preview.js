d1ee40feb85a5085352974aed07091d48e1aac06
Samiya <skmcintosh20@gmail.com>
2022-04-10 17:12:12 -0700

    Built the Preview Screen/ added posts to firebase

--- src/Preview.js
+++ src/Preview.js
@@ -1,15 +1,83 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { selectCameraImage } from "./features/cameraSlice";
+import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { resetCameraImage, selectCameraImage } from "./features/cameraSlice";
+import CloseIcon from "@material-ui/icons/Close";
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import NoteIcon from "@material-ui/icons/Note";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import CropIcon from "@material-ui/icons/Crop";
+import TimerIcon from "@material-ui/icons/Timer";
+import CreateIcon from "@material-ui/icons/Create";
+import SendIcon from "@material-ui/icons/Send";
 import "./Preview.css";
+import { v4 as uuid } from "uuid";
+import { storage, db } from "./firebase";
+import firebase from "firebase";
 
 function Preview() {
 	const cameraImage = useSelector(selectCameraImage);
+	const history = useHistory();
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (!cameraImage) {
+			history.replace("/");
+		}
+	}, [cameraImage, history]);
+
+	const closePreview = () => {
+		dispatch(resetCameraImage());
+	};
+
+	const sendPost = () => {
+		const id = uuid();
+		const uploadTask = storage
+			.ref(`posts/${id}`)
+			.putString(cameraImage, "data_url");
+
+		uploadTask.on(
+			"state_changed",
+			null,
+			(error) => {
+				console.log(error);
+			},
+			() => {
+				storage
+					.ref("posts")
+					.child(id)
+					.getDownloadURL()
+					.then((url) => {
+						db.collection("posts").add({
+							imageUrl: url,
+							username: "Samiya",
+							read: false,
+							timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+						});
+						history.replace("/chats");
+					});
+			}
+		);
+	};
 
 	return (
 		<div className="preview">
-			<h2>THIS IS YOUR PREVIEW</h2>
+			<CloseIcon onClick={closePreview} className="preview_close" />
+			<div className="preview_toolbarRight">
+				<TextFieldsIcon />
+				<CreateIcon />
+				<NoteIcon />
+				<MusicNoteIcon />
+				<AttachFileIcon />
+				<CropIcon />
+				<TimerIcon />
+			</div>
 			<img src={cameraImage} alt="" />
+			<div onClick={sendPost} className="preview_footer">
+				<h2>Send Now</h2>
+				<SendIcon fontSize="small" className="preview_sendIcon" />
+			</div>
 		</div>
 	);
 }
